08062cf7d8375a5d277059a54a3f2afbe9440bcd
Panito115 <jpmadriz@ufm.edu>
2026-01-26 10:49:01 -0600

    se comento lo que jan pidio

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -36,43 +36,45 @@ export const USERNAME_REGEX = /^[a-zA-Z0-9_]{3,30}$/;
  * Validaciones básicas
  */
 
-export function normalizeUsername(username:string): string {
-    return username.trim();
-}
 
-export function normalizeEmail(email:string): string {
-    return email.trim().toLocaleLowerCase()
-}
 
-export function validateUsername(username:string): string {
-    const value = normalizeUsername(username);
-    if (!USERNAME_REGEX.test(value)) { 
-        throw new Error("Invalid username: must be 3–30 characters and contain only letters, numbers or '_'");
-    }
-    return value;
-}
+// export function normalizeUsername(username:string): string {
+//     return username.trim();
+// }
 
-export function validateEmail(email: string): string {
-  const value = normalizeEmail(email);
+// export function normalizeEmail(email:string): string {
+//     return email.trim().toLocaleLowerCase()
+// }
 
-  if (
-    value.length < 3 ||
-    value.length > 320 ||
-    !value.includes("@") ||
-    value.startsWith("@") ||
-    value.endsWith("@")
-  ) {
-    throw new Error("Invalid email format");
-  }
+// export function validateUsername(username:string): string {
+//     const value = normalizeUsername(username);
+//     if (!USERNAME_REGEX.test(value)) { 
+//         throw new Error("Invalid username: must be 3–30 characters and contain only letters, numbers or '_'");
+//     }
+//     return value;
+// }
 
-  return value;
-}
+// export function validateEmail(email: string): string {
+//   const value = normalizeEmail(email);
 
-export function buildUser(input: CreateUserInput): Omit<User, "id"> {
-  return {
-    username: validateUsername(input.username),
-    email: validateEmail(input.email),
-    passwordHash: input.passwordHash,
-    createdAt: new Date(),
-  };
-}
+//   if (
+//     value.length < 3 ||
+//     value.length > 320 ||
+//     !value.includes("@") ||
+//     value.startsWith("@") ||
+//     value.endsWith("@")
+//   ) {
+//     throw new Error("Invalid email format");
+//   }
+
+//   return value;
+// }
+
+// export function buildUser(input: CreateUserInput): Omit<User, "id"> {
+//   return {
+//     username: validateUsername(input.username),
+//     email: validateEmail(input.email),
+//     passwordHash: input.passwordHash,
+//     createdAt: new Date(),
+//   };
+// }
