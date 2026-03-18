de1472520778115c5b06551020c75247c273fc7f
anasofiaeggenberger <156016977+anasofiaeggenberger@users.noreply.github.com>
2026-03-17 22:23:28 -0600

    implement follow/unfollow functionality with endpoints, service, and repository

--- backend/src/init-db.ts
+++ backend/src/init-db.ts
@@ -73,6 +73,19 @@ async function initDb() {
       ) ENGINE=InnoDB;
     `);
 
+        // Tabla de follows
+    await connection.query(`
+  CREATE TABLE IF NOT EXISTS followers (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    follower_id INT NOT NULL,
+    following_id INT NOT NULL,
+    createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE KEY unique_follow (follower_id, following_id),
+    FOREIGN KEY (follower_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (following_id) REFERENCES users(id) ON DELETE CASCADE
+  ) ENGINE=InnoDB;
+`);
+
     console.log(`✅ Base de datos "${dbName}" y tablas aseguradas.`);
 
     await connection.end();
